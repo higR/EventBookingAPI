992d52b8ea9d7887a46316bcb0accc934a36d50e
Higor Oliveira <higoroliveira75@gmail.com>
2025-04-12 18:49:25 +0100

    Initial commit

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using EventBookingAPI.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventBookingAPI.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEvents()
+        {
+            var events = await _eventService.GetAllEventsAsync();
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEvent(int id)
+        {
+            var eventEntity = await _eventService.GetEventByIdAsync(id);
+            return eventEntity == null ? NotFound() : Ok(eventEntity);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using EventBookingAPI.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Other services (like controllers, etc.)
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
